827432155ab56c9d1c86a316f398fad2c9d9a333
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2016-11-19 23:10:56 +0500

    Added support callback

--- src/response_time.cr
+++ src/response_time.cr
@@ -4,7 +4,15 @@ require "./response_time/*"
 
 module ResponseTime
   class Handler < HTTP::Handler
+    alias CALLBACK = HTTP::Server::Context, Time::Span ->
+
+    @callback = nil
+
     def initialize(@header = "X-Response-Time", @suffix = true)
+    end
+
+    def initialize(@header = "X-Response-Time", @suffix = true, &block : CALLBACK)
+      @callback = block
     end
 
     def call(context)
@@ -16,6 +24,10 @@ module ResponseTime
 
       millis = elapsed.total_milliseconds.to_s
       context.response.headers[@header] = @suffix ? "#{millis}ms" : millis
+
+      if callback = @callback
+        callback.call(context, elapsed)
+      end
     end
   end
 end
